18b107cce4bed40293e0ef7f995e5686d65d082b
Vicentee11 <vicentecarrilloo10@gmail.com>
2022-10-08 18:07:20 +0200

    Perfil animal

--- App.js
+++ App.js
@@ -10,15 +10,18 @@ import InicioSesion from './screens/InicioSesion'
 import RegistrarUsuario from './screens/RegistrarUsuario'
 import PrincipalScreen from './screens/PrincipalScreen'
 import ListaProtectoras from './screens/ListaProtectoras'
-
+import ListaAnimales from './screens/ListaAnimales';
+import PerfilAnimal from './screens/PerfilAnimal';
 
 function MyStack (){
   return (
     <Stack.Navigator>
+      <Stack.Screen name="ListaAnimales" component={ListaAnimales} />
       <Stack.Screen name="ListaProtectoras" component={ListaProtectoras}/>
       <Stack.Screen name="PrincipalScreen" component={PrincipalScreen}/>
       <Stack.Screen name="InicioSesion" component={InicioSesion} />
       <Stack.Screen name="RegistrarUsuario" component={RegistrarUsuario} />
+      <Stack.Screen name="PerfilAnimal" component={PerfilAnimal} />
     </Stack.Navigator>
   )
 }
---
+++ screens/ListaAnimales.js
@@ -0,0 +1,86 @@
+import React, {useEffect, useState} from "react";
+import { View, Button, TextInput, Text, StyleSheet, ScrollView, ProgressViewIOSComponent} from "react-native";
+import firebase from '../database/firebase';
+import {Avatar, ListItem} from "react-native-elements";
+
+const ListaAnimales = (props) => {
+
+    const [animales, setProtectoras] = useState([]);
+
+    useEffect(() => {
+        firebase.db.collection('animales').onSnapshot((querySnapshot) => {
+            const listaAnimales = []
+
+            querySnapshot.docs.forEach((doc) => {
+                const {nombre, raza, sexo, edad, descripcion, fecha_nacimiento} = doc.data()
+                listaAnimales.push({
+                    id: doc.id,
+                    nombre,
+                    raza,
+                    sexo,
+                    edad,
+                    descripcion,
+                    fecha_nacimiento
+                })
+            });
+            setProtectoras(listaAnimales)
+        })
+    })
+
+    return(
+        <ScrollView>
+            <Text style={styles.titulo}>
+                Lista Animales
+            </Text>
+            { animales.map((animal) => {
+                return(
+            <ListItem key={animal.id} 
+            bottomDivider
+            onPress={() => {
+                props.navigation.navigate("PerfilAnimal", {
+                  animalId: animal.id,
+                });
+              }}
+            >
+                <ListItem.Chevron />
+                <ListItem.Content 
+                style={styles.lista}>
+                    <ListItem.Title 
+                    style={{fontWeight: "bold"}}> 
+                        {animal.nombre} 
+                    </ListItem.Title>
+                    <ListItem.Subtitle> {animal.descripcion} </ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem>
+            )})
+            }
+        </ScrollView>
+    )
+}
+
+const styles = StyleSheet.create({
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },
+    titulo: {
+        margin: 12,
+        padding: 10,
+        fontSize: 40,
+        fontWeight: 'bold',
+        textAlign: "left"
+    },
+    lista: {
+        margin: 12,
+        padding: 10
+    },
+    imagen: {
+        height: 60,
+        width: 60
+    }
+
+});
+
+export default ListaAnimales;
