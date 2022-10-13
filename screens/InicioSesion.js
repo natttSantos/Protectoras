4df19d5be6b3eeb51b4168393c1e19e1cf490f6e
Carla Lixiang <carla20011001@gmail.com>
2022-10-13 19:22:58 +0200

    LogIn con alerta y contemplando protectoras
    LogIn con un mensaje de alerta en caso de que no se encuentren ni el email ni la contraseña y permitiendo iniciar sesión como una protectora

--- screens/InicioSesion.js
+++ screens/InicioSesion.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import { View, Button, TextInput, Text,StyleSheet, ScrollView, ProgressViewIOSComponent} from "react-native";
+import { View, Button, TextInput, Text,StyleSheet, ScrollView, ProgressViewIOSComponent, Alert} from "react-native";
 import firebase from '../database/firebase';
 
 const InicioSesion = (props) => {
@@ -29,6 +29,27 @@ const InicioSesion = (props) => {
                         userId: doc.id
                     })
                 } 
+                else {
+                    Alert.alert("Error", "El usuario o la contraseña son incorrectas", [
+                        {text: "Cerrar"}
+                    ]);
+                }
+            })
+        }));
+        dbRef = firebase.db.collection('protectoras').onSnapshot((querySnapchot => {
+
+            querySnapchot.docs.forEach((doc) => {
+                const {email, contraseña} = doc.data();
+                if (email == state.email && contraseña == state.contraseña) {
+                    props.navigation.navigate('UserDetailScreen', {
+                        userId: doc.id
+                    })
+                } 
+                else {
+                    Alert.alert("Error", "El usuario o la contraseña son incorrectas", [
+                        {text: "Cerrar"}
+                    ]);
+                }
             })
         }));
     };
@@ -52,7 +73,7 @@ const InicioSesion = (props) => {
             </View>
             <View>
                 <Button title="Iniciar sesion"
-                onPress={() => validateUser()}
+                onPress={() => validateUser() }
                 />
             </View>
         </ScrollView>
