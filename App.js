359377bfaaedb9e01c227ba82a4987556e68a612
Devalent20 <99796127+Devalent20@users.noreply.github.com>
2022-10-15 14:23:38 +0200

    Conflictos merge arreglados

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ import PerfilAnimal from './screens/PerfilAnimal';
 import SesionUsuario from './screens/SesionUsuario';
 import PerfilUsuario from './screens/PerfilUsuario';
 import PerfilProtectora from './screens/PerfilProtectora';
+import UserDetailScreen from './screens/UserDetailScreen';
 
 
 
@@ -32,6 +33,7 @@ function MyStack (){
       <Stack.Screen name="SesionUsuario" component={SesionUsuario} />
       <Stack.Screen name="PerfilUsuario" component={PerfilUsuario} />
       <Stack.Screen name="PerfilProtectora" component={PerfilProtectora} />
+      <Stack.Screen name="UserDetailScreen" component={UserDetailScreen} />
     </Stack.Navigator>
   )
 }
