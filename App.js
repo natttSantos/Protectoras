7cc99ff3ac474b318f091aed3e80864b2e38df84
Jordi <jarncal@upv.edu.es>
2022-11-23 09:38:33 +0100

    Ignorar los warnings

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, LogBox, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack'; 
 
@@ -8,6 +8,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, {useState} from 'react';
 import { CredentialsContext } from './components/CredentialsContext';
 import RootStack from './navigators/RootStack';
+
+LogBox.ignoreAllLogs();
 
 const Stack = createNativeStackNavigator()
 
